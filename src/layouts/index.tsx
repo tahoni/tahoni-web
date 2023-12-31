357362f82e74f5ca05e9d29fd67cec7f2a313046
Leoni Lubbinge <tahoni@gmail.com>
2023-12-31 21:54:50 +0200

    Basic skeleton page with cover image

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,15 +1,16 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Container, Image } from "react-bootstrap";
 import classes from './index.module.scss';
+import WaterLilyCover from "../assets/img/tahoni-cover-water-lilies.jpg";
 
 export const Layout = (): React.ReactElement => {
     return (
-        <Container className={classes.Layout}>
-            <header className={classes.Header}>
-                Here
-            </header>
+        <Container fluid className={classes.Layout}>
             <main className={classes.Main}>
-                There
+                <div className={classes.Cover}>
+                    <Image src={WaterLilyCover} alt="" />
+                    <span className="cover-text">My Coding Journey</span>
+                </div>
             </main>
             <footer className={classes.Footer}>
                 Here
